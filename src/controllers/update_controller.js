ff0ff9a298e2586000e7830bf5f88d7b2d36b9d8
Lucas Magalhães <lucaspokaz@gmail.com>
2020-10-07 13:37:35 -0300

    aplicando deploy para nfacil

--- src/controllers/update_controller.js
+++ src/controllers/update_controller.js
@@ -26,6 +26,18 @@ exports.get_versions_siembra = async (req, res, next) => {
 
 };
 
+exports.get_versions_nfacil = async (req, res, next) => {
+
+	let retorno = await updatesService.get_versions('nfacil');
+	let dados = await JSON.parse(retorno);
+
+	res.render('updates/nfacil', {
+		data: dados,
+		moment: require('moment')
+	});
+
+};
+
 exports.load = async (req, res, next) => {
 
 	// await new Promise(r => setTimeout(r, 2000));
@@ -34,7 +46,7 @@ exports.load = async (req, res, next) => {
 
 	res.render('updates/elysius_deploy', {
 		session: req.session,
-		menu: 'lista_updates',
+		menu: 'lista_updates_elysius',
 		formatter: require('../utils/formatter'),
 		data: dados,
 		moment: require('moment')
@@ -50,7 +62,23 @@ exports.load_siembra = async (req, res, next) => {
 
 	res.render('updates/siembra_deploy', {
 		session: req.session,
-		menu: 'lista_updates',
+		menu: 'lista_updates_siembra',
+		formatter: require('../utils/formatter'),
+		data: dados,
+		moment: require('moment')
+	});
+
+};
+
+exports.load_nfacil = async (req, res, next) => {
+
+	// await new Promise(r => setTimeout(r, 2000));
+	let retorno = await updatesService.get_update_files('nfacil');
+	dados = await JSON.parse(retorno);
+
+	res.render('updates/nfacil_deploy', {
+		session: req.session,
+		menu: 'lista_updates_nfacil',
 		formatter: require('../utils/formatter'),
 		data: dados,
 		moment: require('moment')
@@ -80,6 +108,17 @@ exports.get_files_versions_siembra = async (req, res, next) => {
 	}
 };
 
+exports.get_files_versions_nfacil = async (req, res, next) => {
+	let retorno = await updatesService.get_update_files('nfacil');
+	let dados = await JSON.parse(retorno);
+	console.log(utils.data_atual());
+	try {
+		res.status(200).send(dados)
+	} catch (error) {
+		res.status(500).send('Erro ao consultar os dados.')
+	}
+};
+
 exports.get_files_especials_elysius = async (req, res, next) => {
 	let retorno = await updatesService.get_update_files_especials('elysius');
 	let dados = await JSON.parse(retorno);
@@ -92,6 +131,16 @@ exports.get_files_especials_elysius = async (req, res, next) => {
 
 exports.get_files_especials_siembra = async (req, res, next) => {
 	let retorno = await updatesService.get_update_files_especials('siembra');
+	let dados = await JSON.parse(retorno);
+	try {
+		res.status(200).send(dados)
+	} catch (error) {
+		res.status(500).send('Erro ao consultar os dados.')
+	}
+};
+
+exports.get_files_especials_nfacil = async (req, res, next) => {
+	let retorno = await updatesService.get_update_files_especials('nfacil');
 	let dados = await JSON.parse(retorno);
 	try {
 		res.status(200).send(dados)
@@ -172,6 +221,33 @@ exports.upload_siembra = async (req, res) => {
 			//Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
 			let file = req.files.file;
 			let dest_file = '../../public_html/downloads/siembra/' + file.name;
+
+			//Use the mv() method to place the file in upload directory (i.e. "uploads")
+			await file.mv(dest_file);
+
+			//send response
+			res.send({
+				status: true, message: 'Atualizado com sucesso',
+				data: { name: file.name, mimetype: file.mimetype, size: file.size }
+			});
+		}
+	} catch (err) {
+		res.status(500).send(err);
+	}
+
+};
+
+exports.upload_nfacil = async (req, res) => {
+
+	try {
+
+		if (!req.files) {
+			res.send({ status: false, message: 'Erro ao atualizar' });
+		} else {
+
+			//Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+			let file = req.files.file;
+			let dest_file = '../../public_html/downloads/nfacil/' + file.name;
 
 			//Use the mv() method to place the file in upload directory (i.e. "uploads")
 			await file.mv(dest_file);
--- src/routes/update_route.js
+++ src/routes/update_route.js
@@ -10,12 +10,18 @@ router.get('/elysius/deploy', controller.load);
 router.get('/siembra', controller.get_versions_siembra);
 router.get('/siembra/deploy', controller.load_siembra);
 
+router.get('/nfacil', controller.get_versions_nfacil);
+router.get('/nfacil/deploy', controller.load_nfacil);
+
 // json
 router.get('/elysius/arquivos/json', controller.get_files_versions_elysius);
 router.get('/elysius/especiais/json', controller.get_files_especials_elysius);
 
 router.get('/siembra/arquivos/json', controller.get_files_versions_siembra);
 router.get('/siembra/especiais/json', controller.get_files_especials_siembra);
+
+router.get('/nfacil/arquivos/json', controller.get_files_versions_nfacil);
+router.get('/nfacil/especiais/json', controller.get_files_especials_nfacil);
 
 // post
 router.post('/elysius', controller.create_replace);
@@ -25,5 +31,9 @@ router.post('/elysius/arquivos', controller.upload_elysius);
 router.post('/siembra', controller.create_replace);
 router.post('/siembra/historico', auth.isValid, controller.create_history);
 router.post('/siembra/arquivos', controller.upload_siembra);
+
+router.post('/nfacil', controller.create_replace);
+router.post('/nfacil/historico', auth.isValid, controller.create_history);
+router.post('/nfacil/arquivos', controller.upload_nfacil);
 
 module.exports = router;
