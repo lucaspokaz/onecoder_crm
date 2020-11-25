4997d0fed8f9d44992d474ef37408f5fa88e1295
Lucas Magalhães <lucaspokaz@gmail.com>
2020-11-25 13:39:33 -0300

    alterando exibição dos updates do elysius

--- public/app-assets/js/scripts/pages/timeline.js
+++ public/app-assets/js/scripts/pages/timeline.js
@@ -16,21 +16,4 @@
         radioClass: 'iradio_square-blue',
     });
 
-    // Basic Map
-    // ------------------------------
-    var movedBrooklyn =  new GMaps({
-        div: '#moved-brooklyn',
-        lat: 40.650002,
-        lng: -73.949997,
-        height: 400,
-    });
-    movedBrooklyn.addMarker({
-        lat: 40.650002,
-        lng: -73.949997,
-        title: 'Moved Brooklyn',
-        infoWindow: {
-            content: '<h3>Moved Brooklyn</h3> <p>Our new office with more team members.</p>'
-        }
-    });
-
 })(window, document, jQuery);
